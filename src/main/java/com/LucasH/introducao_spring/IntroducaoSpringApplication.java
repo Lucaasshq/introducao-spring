5e692e0bc16c1dfac0ea133d699e5d0331b5c778
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-22 17:51:08 -0300

    initial commit

--- src/main/java/com/LucasH/introducao_spring/IntroducaoSpringApplication.java
+++ src/main/java/com/LucasH/introducao_spring/IntroducaoSpringApplication.java
@@ -1,13 +1,26 @@
 package com.LucasH.introducao_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IntroducaoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroducaoSpringApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\":\"Praca de sé\",\"localidade\":\"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do cep: " + response);
+
+		};
 	}
 
 }
---
+++ src/main/java/com/LucasH/introducao_spring/MyApp.java
@@ -0,0 +1,14 @@
+package com.LucasH.introducao_spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
